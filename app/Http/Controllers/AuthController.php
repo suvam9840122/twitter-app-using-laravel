65a736f6926e6012856da414fdce1db1bf5f8f43
Suvam <suvamxtha10@gmail.com>
2026-08-19 17:10:54 +0545

    Add login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,11 +9,13 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register() {
-    return view('auth.register');
+    public function register()
+    {
+        return view('auth.register');
     }
 
-    public function store() {
+    public function store()
+    {
         $validated = request()->validate(
             [
                 'name' => 'required|min:3|max:30',
@@ -28,8 +30,43 @@ class AuthController extends Controller
                 'password' => Hash::make($validated['password']),
 
             ]
-            );
-        return redirect()->route('dashboard')->with('success','Account Created Successfully!');
+        );
+        return redirect()->route('dashboard')->with('success', 'Account Created Successfully!');
+    }
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate()
+    {
+        $validated = request()->validate(
+            [
+
+                'email' => 'required|email',
+                'password' => 'required'
+            ]
+        );
+        if (auth()->attempt($validated)) {
+
+            request()->session()->regenerate();
+
+            return redirect()->route('dashboard')->with('success', 'Logged In Successfully!');
+        }
+        return redirect()->route('login')->withErrors(
+            [
+                'email' => "No matching user found with the provided email and password"
+            ]
+        );
+    }
+
+    public function logout() {
+        
+        auth()->logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect()->route('dashboard')->with('success','Logged out successfully');
     }
 }
-
